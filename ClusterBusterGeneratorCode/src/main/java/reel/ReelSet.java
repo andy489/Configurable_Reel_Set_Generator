1d6af57448239818e9314f92f9c4f28e2134c78d
stoev.andy <astoev@evolution.com>
2022-09-04 00:10:10 +0300

    Modify: add flat strategy and create jar

---
+++ ClusterBusterGeneratorCode/src/main/java/reel/ReelSet.java
@@ -0,0 +1,46 @@
+package reel;
+
+import com.fasterxml.jackson.annotation.JsonAlias;
+import com.fasterxml.jackson.annotation.JsonProperty;
+
+import java.util.List;
+
+public class ReelSet {
+    @JsonAlias({"counts", "cnt", "cnts", "tiles"})
+    protected List<List<Integer>> tilesCounts;
+    @JsonAlias({"res"})
+
+    protected List<Restriction> restrictions;
+
+    @Override
+    public String toString() {
+        return "ReelSet{" +
+                "tilesCounts=" + tilesCounts +
+                ", restrictions=" + restrictions +
+                '}';
+    }
+
+    public ReelSet() {
+    }
+
+    public ReelSet(List<List<Integer>> tilesCounts, List<Restriction> restrictions) {
+        this.tilesCounts = tilesCounts;
+        this.restrictions = restrictions;
+    }
+
+    public void setTilesCounts(List<List<Integer>> tilesCounts) {
+        this.tilesCounts = tilesCounts;
+    }
+
+    public void setRestrictions(List<Restriction> restrictions) {
+        this.restrictions = restrictions;
+    }
+
+    public List<List<Integer>> getTilesCounts() {
+        return tilesCounts;
+    }
+
+    public List<Restriction> getRestrictions() {
+        return restrictions;
+    }
+}
